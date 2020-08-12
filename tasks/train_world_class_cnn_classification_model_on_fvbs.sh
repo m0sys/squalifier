a804a245aa0b5c533c443402cfd3d2d4f8fcec82
mhd53 <mhd53@users.noreply.github.com>
2020-08-12 07:42:41 -0700

    ran first experiment

---
+++ tasks/train_world_class_cnn_classification_model_on_fvbs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+python training/run_experiment.py --save '{"dataset": "FvbsDataset",
+                                           "model": "CnnClassificationModel",
+                                           "network": "resnet34",
+                                           "train_args": {
+                                             "stage_one": {
+                                               "one_cycle": 1,
+                                               "epochs": 10},
+                                             "stage_two": {
+                                               "unfreeze": 1,
+                                               "one_cycle": 1,
+                                               "max_lr_start": 3e-4,
+                                               "max_lr_end": 3e-3,
+                                               "epochs": 15
+                                             }
+                                           }
+                                          }'
