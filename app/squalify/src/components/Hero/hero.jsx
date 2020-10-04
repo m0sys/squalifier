f9a9bd7220aa543efa5099e350225551de518dea
mhd53 <mhd53@users.noreply.github.com>
2020-10-04 10:27:08 -0400

    feat: add desktop layout

--- app/squalify/src/components/Hero/hero.jsx
+++ app/squalify/src/components/Hero/hero.jsx
@@ -1,66 +1,56 @@
-import React from 'react';
+import React from "react";
 import { useStaticQuery, graphql, navigate } from "gatsby";
 import Img from "gatsby-image";
 
-import Container from "../Container/container";
+import Grid from "../Grid/grid";
 import Button from "../Button/button";
 
 import heroStyles from "./hero.module.scss";
 
-
 const Hero = () => {
-  const data = useHeroImage()
+  const data = useHeroImage();
 
   function useHeroImage() {
     return useStaticQuery(graphql`
-    query {
-      file(relativePath: {
-        eq: "hero.jpg"
-      })
-      {
-        childImageSharp {
-          fluid(maxWidth: 1500, quality: 100) {
-            ...GatsbyImageSharpFluid
-            
+      query {
+        file(relativePath: { eq: "hero.jpg" }) {
+          childImageSharp {
+            fluid(maxWidth: 1500, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
           }
         }
       }
-    }
-    `)
+    `);
   }
 
   function handlOnClick() {
-    navigate('/classification')
+    navigate("/classification");
   }
 
-
   return (
-  <main className={heroStyles.hero}>
-    <Container>
-      <h3>
-        Welcome to <span>Squalify</span>
-      </h3>
-      <h2>
-        The First AI Powered <span>Front VS Back Squat Classifier!</span>
-      </h2>
-      <div className={heroStyles.heroImgContainer}>
-        <Img className={heroStyles.heroImg}  fluid={data.file.childImageSharp.fluid}/>
-        <p className="meta-text">
-          Photo by{" "}
-          <a href="https://unsplash.com/@amandhakal?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
-            Aman Dhakal
-          </a>{" "}
-          on{" "}
-          <a href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">
-            Unsplash
-          </a>
-        </p>
-      </div>
-      <Button text="Initiate Classification" onClick={handlOnClick} />
-    </Container>
-  </main>
+    <main className={heroStyles.hero}>
+      <Grid hasMinHeight center>
+        <div className={heroStyles.top}>
+          <h3 className={heroStyles.subhead}>
+            Welcome to <span>Squalify</span>
+          </h3>
+          <h2 className={heroStyles.headline}>
+            The First AI Powered <span>Front VS Back Squat Classifier!</span>
+          </h2>
+          <div className={heroStyles.cta}>
+            <Button text="Initiate Classification" onClick={handlOnClick} />
+          </div>
+        </div>
+        <div className={heroStyles.heroImgContainer}>
+          <Img
+            className={heroStyles.heroImg}
+            fluid={data.file.childImageSharp.fluid}
+          />
+        </div>
+      </Grid>
+    </main>
   );
 };
-
 
 export default Hero;
--- app/squalify/src/pages/classification.jsx
+++ app/squalify/src/pages/classification.jsx
@@ -1,25 +1,43 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 
 import Header from "../components/Header/header";
-import Container from "../components/Container/container";
+import Grid from "../components/Grid/grid";
 import Button from "../components/Button/button";
 import ClassOptions from "../components/ClassOptions/class_options";
 import Upload from "../components/Upload/upload";
 
+import classificationStyles from "../styles/classification.module.scss";
 import "../styles/main.scss";
 
 export default function ClassificationPage() {
-  
+  const [image, setImage] = useState(null);
+
+  function onUploadCompleted(res) {
+    console.log("Upload Completed");
+    console.log(res);
+    setImage(res);
+  }
+
+  function onHandleClick() {
+    console.log("Classify!");
+  }
   return (
     <div>
       <Header />
-      <Container>
-        <h3>Front Squat or Back Squat?</h3>
-        <Upload />
-        <Button text="Classify" />
-        <ClassOptions />
-      </Container>
+      <Grid hasMinHeight center>
+        <h3 className={classificationStyles.headline}>
+          Front Squat or Back Squat?
+        </h3>
+        <div className={classificationStyles.upload}>
+          <Upload onUploadCompleted={onUploadCompleted} />
+        </div>
+        <div className={classificationStyles.cta}>
+          <Button onClick={onHandleClick} text="Classify" />
+        </div>
+        <div className={classificationStyles.options}>
+          <ClassOptions />
+        </div>
+      </Grid>
     </div>
-
-  )
+  );
 }
