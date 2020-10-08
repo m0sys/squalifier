d6d6dae1e03155a0b61e378a07bac61a86ba8293
mhd53 <mhd53@users.noreply.github.com>
2020-10-08 09:48:14 -0400

    feat: add cors for dev + netlify site

--- api/graphql_app.py
+++ api/graphql_app.py
@@ -1,6 +1,7 @@
 """Define GraphQL server."""
 import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from ariadne import load_schema_from_path, make_executable_schema, snake_case_fallback_resolvers, upload_scalar
 
 from ariadne.asgi import GraphQL
@@ -9,6 +10,7 @@ import uvicorn
 from api.resolvers.query import query
 from api.resolvers.mutation import mutation
 
+origins = ["http://localhost:8000", "http:/localhost:8080", "https://squalify.netlify.app"]
 
 app = FastAPI()
 
@@ -22,6 +24,8 @@ async def root():
 
 
 app.add_route("/graphql", GraphQL(schema=schema, debug=True))
+
+app.add_middleware(CORSMiddleware, allow_origins=origins, allow_methods=["*"], allow_headers=["*"])
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT")))
