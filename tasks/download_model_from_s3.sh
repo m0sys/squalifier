6f4b4c64b94a907e09ddf8d5692f6cf32b0c1b4b
mhd53 <mhd53@users.noreply.github.com>
2020-10-08 15:23:04 -0400

    fix(cd): add model download to deploy workflow

--- tasks/download_model_from_s3.sh
+++ tasks/download_model_from_s3.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 MODEL_PATH="./squat_recognizer/exports/"
 MODEL_NAME="2020-09-23_12-19-00_CnnClassificationModel_FvbsDataset_resnet34.pkl"
+SAVE_MODEL_NAME="CnnClassificationModel_FvbsDataset_resnet34.pkl"
 
-aws s3 cp "s3://squalifier/trained-models/""${MODEL_NAME}" "${MODEL_PATH}""${MODEL_NAME}"
+aws s3 cp "s3://squalifier/trained-models/""${MODEL_NAME}" "${MODEL_PATH}""${SAVE_MODEL_NAME}"
