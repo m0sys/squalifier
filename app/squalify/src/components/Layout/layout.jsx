2b054e00ddbf8e82017e14afd30ba434a1308755
mhd53 <mhd53@users.noreply.github.com>
2020-10-09 10:08:26 -0400

    feat: add footer component

--- app/squalify/src/components/Layout/layout.jsx
+++ app/squalify/src/components/Layout/layout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../Header/header";
+import Footer from "../Footer/footer";
 
 const Layout = props => {
   const { children } = props;
@@ -8,6 +9,7 @@ const Layout = props => {
     <React.Fragment>
       <Header />
       {children}
+      <Footer />
     </React.Fragment>
   );
 };
